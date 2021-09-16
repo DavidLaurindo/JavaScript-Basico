57c7cb9a54cb74a80ee1b0107a7e3d61b17de6d0
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-16 10:38:00 -0300

    operadores logigos, atribuiões e conversão de valores

---
+++ codigos/converterParaNumero.js
@@ -0,0 +1,9 @@
+let numero = '5'; //String
+
+console.log(typeof numero);
+
+numero = parseInt(numero); //modificou para number
+
+console.log(typeof numero);
+
+//também pode usar Number('8');
---
+++ codigos/operadoresAritimeticos.js
@@ -0,0 +1,42 @@
+/*
+NaN = não é um numero.
+
++ adição / concatenação
+- diminuir
+* multiplicar
+/ dividir 
+
+** pontencializar
+% retorna o resto da divisão
+
+------------------------------------------------------------
+
+Procedencia:
+1 ( ) entre parêntese será executado primeiro.
+2 **
+3 * / %
+4 + -
+
+--------------------------------------------------------------
+
+operadores de atribuição: 
+
+++ operador de incremento.(não utilizar em variável const).
+ex: numero++ ou ++numero. pode usar antes ou depois.
+
+-- operador de decremento.(não utilizar em variável const).
+ex: numero-- ou --numero . pode usar antes ou depois.
+
++= (contador = contador + numero).
+*= (contador = contador * numero).
+**= (contador = contador ** numero). potencialização.
+
+-----------------------------------------------------------------------
+
+converter:
+parseInt('5');
+parseFloat()
+Number()
+
+*/
+
