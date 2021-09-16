3fc44cb625a98d47c26a75fb699816095291ec19
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-15 23:42:26 -0300

    adicionando.comentarios

---
+++ codigos/comentario.js
@@ -0,0 +1,19 @@
+// Escrever comentário aqui( só funciona nesta linha).
+console.log('teste');//essa parte é ignorada.
+
+/**comentário para várias linhas
+*linha 02
+*linha 04
+*linha 05
+*linha 06
+*encerrar comentário com o comando
+*/
+console.log('teste');
+
+/*comentário para várias linhas
+linha 02
+linha 04
+linha 05
+linha 06
+encerrar comentário com o comando*/
+console.log('teste');
