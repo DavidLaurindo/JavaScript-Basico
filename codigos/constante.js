3e33eb7067992328377dbae4ea438e8b1e3fa761
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-16 01:40:26 -0300

    constatntes

---
+++ codigos/constante.js
@@ -0,0 +1,15 @@
+/*
+Não podemos mudar o valor de uma const.
+Teremos que criar e obrigado a declarar uma const.
+Não utilize var, UTILIZE const
+*/
+const nome = 'João'; 
+const primeiroNumero = 5;
+const secNumero = 10;
+
+const conta = primeiroNumero * secNumero;
+console.log(conta);
+
+console.log(typeof(secNumero));
+
+//typeof(variável) - para saber qual a variável.
