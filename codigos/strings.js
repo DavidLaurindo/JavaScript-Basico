6fb05af396cd032145b62411519c06332718abf8
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-17 19:43:20 -0300

    sobre Strings

---
+++ codigos/strings.js
@@ -0,0 +1,14 @@
+let umaString = "um texto";
+
+console.log(umaString[4]);
+console.log(umaString.charAt(6));
+console.log(umaString.concat(' teste'));//contatena com o texto inserido;
+console.log(umaString.indexOf('texto'));//mostra qual índice começa a palavra texto.
+console.log(umaString.lastIndexOf('o'));//começa a buscar de traz pra frente.
+console.log(umaString.match());//expressão regular.
+console.log(umaString.search());//retorna o índice onde pediu a letra.
+console.log(umaString.replace('um', 'outro'));//troca a primeira palavra pela segunda.
+console.log(umaString);
+console.log(umaString.length);
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
