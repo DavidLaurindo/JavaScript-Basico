8a251de94e4145242e1f8c4021371e878f6dde8f
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-17 18:26:23 -0300

    exercice basico

--- aula 11/js/script.js
+++ aula 11/js/script.js
@@ -1,1 +1,12 @@
-alert('Nossa mensagem.');
+//alert não existe no node. somente no navegador.
+
+let num1 = prompt('Fala o numero 1');
+let num2 = prompt('Fala o numero 2');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+let soma = num1 + num2;
+
+alert(soma);
+
