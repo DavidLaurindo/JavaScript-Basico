3940ad2a16a5d5e77a799e334867df21eab6ba45
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-20 09:44:28 -0300

    Aula.Math

---
+++ codigos/math.js
@@ -0,0 +1,9 @@
+let num1 = 9.54578;
+
+//let num2 = Math.floor(num1);//arredonda para menos.
+//let num2 = Math.ceil(num2);//arredonda pra mais
+//let num2 = Math.round(num1);//na metade ele arredonda pra mais ou menos dependendo do número.
+//console.log(Math.max(1,2,3,4,5,-40,-50,1500,88,15,12));//informa o maior número;
+//console.log(Math.min(1,2,3,4,5,6,-45,-54));
+const aleatorio = Math.random() * (10 - 5) + 5;//número aleatório entre 10 e 5;
+console.log(Math.random());
