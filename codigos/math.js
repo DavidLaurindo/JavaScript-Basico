7b62c1226c1e6af2802ede73ac3847e64c32f284
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-20 12:28:18 -0300

    exercicio.curso

--- codigos/math.js
+++ codigos/math.js
@@ -5,5 +5,6 @@ let num1 = 9.54578;
 //let num2 = Math.round(num1);//na metade ele arredonda pra mais ou menos dependendo do número.
 //console.log(Math.max(1,2,3,4,5,-40,-50,1500,88,15,12));//informa o maior número;
 //console.log(Math.min(1,2,3,4,5,6,-45,-54));
-const aleatorio = Math.random() * (10 - 5) + 5;//número aleatório entre 10 e 5;
-console.log(Math.random());
+const aleatorio = Math.round(Math.random) * (10 - 5) + 5;//número aleatório entre 10 e 5;
+
+console.log(100/0);//infinity
