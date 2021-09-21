72465d097d5a82a689fff8dbb53a1d2f3618c501
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-21 12:12:37 -0300

    exercicio.final.primeiroModulo

--- aula 20/js/script.js
+++ aula 20/js/script.js
@@ -10,7 +10,17 @@ function meuEscopo(){
        const sobrenome = form.querySelector('.sobrenome');
        const peso = form.querySelector('.peso');
        const altura = form.querySelector('.altura');
-
+       
+       pessoas.push({
+           nome: nome.value,
+           sobrenome: sobrenome.value,
+           peso: peso.value,
+           altura: altura.value
+       });
+       console.log(pessoas);
+       resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} 
+       ${peso.value}
+        ${altura.value}</p>`;
    }
    form.addEventListener('submit', eventoForm);
 }
