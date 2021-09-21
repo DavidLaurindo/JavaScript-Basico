905d41e675573341fa575983acd4ff4280a5db37
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-20 21:11:17 -0300

    exercicio inner.html

--- aula 15/script.js
+++ aula 15/script.js
@@ -3,4 +3,13 @@ const numeroTitulo = document.getElementById('numero-titulo');
 const textoId = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
-textoId.innerHTML = `<p>Seu número + 2 é ${numero + 2}.</p>`;
+
+textoId.innerHTML = '';
+
+textoId.innerHTML += `<p>Raiza quadrada é: ${numero ** 0.5}</p>`;
+textoId.innerHTML += `<p>Seu número + 2 é ${numero + 2}.</p>`;
+textoId.innerHTML += `<p>${numero} é inteiro? ${Number.isInteger(numero)}</p>`;
+textoId.innerHTML += `<p>É NaN? ${Number.isNaN(numero)}</p>`;
+textoId.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
+textoId.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`;
+textoId.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`;
