b86d901d73e81e04cf953efcf2be3a01284c4565
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-22 23:57:26 -0300

    adicionando ifs

--- aula 26/assets/js/main.js
+++ aula 26/assets/js/main.js
@@ -1,7 +1,7 @@
 //capturar evento de submit do formulario:
 const form = document.querySelector('#formulario');
 
-form.addEventListener('submit', function(evento){
+form.addEventListener('submit', function (evento) {
     evento.preventDefault();
     const inputPeso = evento.target.querySelector('#peso');
     const inputAltura = evento.target.querySelector('#altura');
@@ -9,12 +9,12 @@ form.addEventListener('submit', function(evento){
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
 
-    if(!peso){
+    if (!peso) {
         setResultado('peso inválido', false);
         return;
     }
 
-    if(!altura){
+    if (!altura) {
         setResultado('Altura inválida', false);
         return;
     }
@@ -24,17 +24,41 @@ form.addEventListener('submit', function(evento){
 
 });
 
-function getImc(peso, altura){
+function getNivelImc(imc) {
+    const nivel = ['Abaixo do peso', 'peso normal', 'soprepeso', 'obesidade grau 1',
+        'obesidade grau 2', 'obesidade grau 3'];
+
+    if(imc >= 39.9) {
+        return nivel[5];
+    }
+    if(imc >= 34.9) {
+        return nivel[4];
+    }
+    if(imc >= 29.9) {
+        return nivel[3];
+    }
+    if(imc >= 24.9) {
+        return nivel[2];
+    }
+    if (imc >= 18.5) {
+        return nivel[1];
+    }
+    if(imc < 18.5){
+        return nivel[0];
+    }
+}
+
+function getImc(peso, altura) {
     const imc = peso / altura ** 2;
     return imc.toFixed(2);
 }
 
-function criaP(){
+function criaP() {
     const p = document.createElement('p');
     return p;
 }
 
-function setResultado(msg, isValid){
+function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
 
