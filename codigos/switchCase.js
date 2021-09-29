39978ec4944f93d8fbd16c6517287c2bef2a12b6
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-09-29 20:10:58 -0300

    aula switch

---
+++ codigos/switchCase.js
@@ -0,0 +1,37 @@
+function getDiaSemanaTexto(){
+    let diaSemanaTexto;
+
+    switch(diaSemana){
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;    
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+        default: 
+            diaSemanaTexto = 'Data inválida';
+            return diaSemanaTexto;
+    }
+}
+
+const data = new Date('1987-04-20 00:00:00');
+const diaSemana = data.getDay();
+const diaSemanaTexto = getDiaSemanaTexto
+
+console.log(diaSemana, diaSemanaTexto);
+
