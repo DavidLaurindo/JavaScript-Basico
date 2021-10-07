60c4be9d88c023285058577a7ef23693e07e7506
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-10-06 23:08:59 -0300

    dia e horas

--- aula 30/assets/js/main.js
+++ aula 30/assets/js/main.js
@@ -1,1 +1,98 @@
-const h1 = document.querySelector(".container");
+const h1 = document.querySelector(".container"); //Buscando o conatainer no html
+const data = new Date();//Criando objeto Date
+
+function getDia(diaSemana) {//função get para tranformar o dia da semana.
+    let diaSemanaTexto;//armazena o dia em String aqui.
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda-feira';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça-feira';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta-feira';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta-feira';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta-feira';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = 'Data inválida';
+            return diaSemanaTexto;
+    }
+}
+function getMes(mes) {
+    let nomeMes;
+
+    switch (mes) {
+        case 0:
+            nomeMes = 'Janeiro';
+            return nomeMes;
+        case 1:
+            nomeMes = 'Fevereiro';
+            return nomeMes;
+        case 2:
+            nomeMes = 'Março';
+            return nomeMes;
+        case 3:
+            nomeMes = 'Abril';
+            return nomeMes;
+        case 4:
+            nomeMes = 'Maio';
+            return nomeMes;
+        case 5:
+            nomeMes = 'Junho';
+            return nomeMes;
+        case 6:
+            nomeMes = 'Julho';
+            return nomeMes;
+        case 7:
+            nomeMes = 'Agosto';
+            return nomeMes;
+        case 8:
+            nomeMes = 'Setembro';
+            return nomeMes;
+        case 9:
+            nomeMes = 'Outubro';
+            return nomeMes;
+        case 10:
+            nomeMes = 'Novembro';
+            return nomeMes;
+        case 11:
+            nomeMes = 'Dezembro';
+            return nomeMes;
+        default:
+            nomeMes = 'Data inválida';
+            return nomeMes;
+    }
+}
+
+function zeroEsquerda(num) {//adiciona o zero na esquerda.
+    return num >= 10 ? num : `0${num}`;
+}
+
+function criaData(data) {
+    const diaSemana = data.getDay();
+    const numeroMes = data.getMonth();
+
+    const nomeDia = getDia(diaSemana);
+    const nomeMes = getMes(numeroMes);
+
+    return (
+        `${nomeDia}, ${data.getDate()} de ${nomeMes} ` +
+        `de ${data.getFullYear()} ` +
+        `${zeroEsquerda(data.getHours())}:${zeroEsquerda(data.getMinutes())}`
+    );
+}
+
+h1.innerHTML = criaData(data);
