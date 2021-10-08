70803c55784de8111385e5b20ec5021238b08f57
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-10-08 00:24:41 -0300

    lets e const

--- codigos/variaveisPart2.js
+++ codigos/variaveisPart2.js
@@ -1,3 +1,4 @@
 /* 
-let tem escopo de bloco.{dentro de chaves pode-se criar outra let somente para aquele bloco.}
+let tem escopo de bloco.{...bloco}{dentro de chaves pode-se criar outra let somente para aquele bloco.}
+var só te escolpo em função. function(){...bloco}.
 */
