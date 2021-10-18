cd5e2d3b85f39b22c709dbcf2f4c2740a36bfef6
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-10-18 11:47:21 -0300

    dicas desestruturação

--- codigos/desestruturacao.js
+++ codigos/desestruturacao.js
@@ -6,3 +6,16 @@ const desestruturar = [b, c, a];
 [a, b, c] = desestruturar;
 //variavel = arrays
 console.log(a, b, c);
+
+//-------------------------------------------------------------------------------
+ 
+const numeros = [100, 2456, 48693, 44, 57, 68, 7, 811112, 90];
+
+const [primeiroNumero, segundoNumero, ...rest] = numeros;
+
+console.log(primeiroNumero, segundoNumero);//pega somento o 1 e 2 do array.
+console.log(rest);//pega o que restou no array.
+
+const [ , , tres, , cinco, seis, , , nove] = numeros;
+console.log(tres, cinco, nove);
+
