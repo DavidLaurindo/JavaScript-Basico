45cc472476802d9be1f91773e277f66b286af3d1
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-10-25 10:24:57 -0300

    adicionando a aula sobre esturura de repetição

--- codigos/estruturaFor.js
+++ codigos/estruturaFor.js
@@ -6,6 +6,28 @@ for(let i = 0; i <=5; i++){
 
 const frutas = ['maçã', 'pêra', 'uva'];
 
-for(let i = 0; i< frutas.length; i++){
+for(let i = 0; i< frutas.length; i++){//For clássico
     console.log(frutas[i]);
 }
+
+//----------------------------------------------------
+
+//For in - lê os índices ou vetores.
+for(let i in frutas){
+    console.log(frutas[i]);
+}
+
+const pessoa = {
+    nome: 'Luiz',
+    idade: 30
+};
+for(let i in pessoa){
+    console.log(i);
+};
+
+//----------------------------------------------
+
+const nome = 'Renato';
+for(let valor of nome){//For of - pega o valor
+    console.log(valor);
+};
