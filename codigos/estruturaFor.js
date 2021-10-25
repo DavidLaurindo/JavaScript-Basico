43dd8b3efbbf80487d1cbf0dfb8982d5b8a4a0f6
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-10-25 07:26:01 -0300

    aula for

--- codigos/estruturaFor.js
+++ codigos/estruturaFor.js
@@ -3,3 +3,9 @@ for(let i = 0; i <=5; i++){
     console.log(numero);
     numero++;
 }
+
+const frutas = ['maçã', 'pêra', 'uva'];
+
+for(let i = 0; i< frutas.length; i++){
+    console.log(frutas[i]);
+}
