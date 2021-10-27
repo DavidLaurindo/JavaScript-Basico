0e437bb8409a1bdba09a56739bd8af81669e67e1
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2021-10-27 14:14:26 -0300

    tratamento de erros part 1

---
+++ codigos/tratamentoErro.js
@@ -0,0 +1,15 @@
+function soma(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        throw new ReferenceError('x e y precisam ser números.');
+    }
+    return x + y;
+}
+
+try {
+    console.log(soma(1, 9));
+    console.log(soma('1', 8));
+} catch (error) {
+    //    console.log(error);Não é aconselhável lançar o erro assim para o usuário.
+    console.log('Algo deu errado');
+
+};
